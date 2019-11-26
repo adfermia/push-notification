df792783eabd1aaf58da458a09b02136eea29cf7
adrian <adrianfern95@gmail.com>
2019-11-26 12:46:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushNotificationsService } from 'ng-push'; // Importar
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-push';
+  constructor(private  pushNotifications: PushNotificationsService) {
+    this.pushNotifications.requestPermission(); // Me imagino que aqui pedira permiso al usuario para poder enviar notificaciones
+  }
+  notify() { // our function to be called on click
+    let options = { // set options
+      body: 'Enhorabuena! Has mutilado a una feminazi!',
+      icon: 'assets/vaultBoy.jpg' // adding an icon
+
+    };
+    this.pushNotifications.create('Vault Boy', options).subscribe(// creates a notification
+        res => console.log(res),
+        err => console.log(err)
+      );
+  }
 }
